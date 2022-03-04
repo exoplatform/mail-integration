637f86b21cb6a5a1e618a71b2581585ed27d9667
Houssem Ben Ali <hbenali@exoplatform.com>
2022-03-04 19:55:53 +0100

    TASK-54977: Initialize project

---
+++ mail-integration-webapp/webpack.prod.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const config = {
+  context: path.resolve(__dirname, '.'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'eslint-loader',
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader',
+          'eslint-loader',
+        ]
+      }
+    ]
+  },
+  entry: {
+    mailintegration: './src/main/webapp/vue-app/mail-integration/main.js',
+  },
+  output: {
+    path: path.join(__dirname, 'target/mail-integration/'),
+    filename: 'js/[name].bundle.js',
+    libraryTarget: 'amd'
+  },
+  externals: {
+    vue: 'Vue',
+    vuetify: 'Vuetify',
+    jquery: '$',
+  },
+};
+
+module.exports = config;
