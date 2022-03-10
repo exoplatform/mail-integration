77b140d02d8a7073e61cc366de53068c6a2021df
Hassen El kamel <54801416+hassenelkamel-exo@users.noreply.github.com>
2022-03-10 17:59:52 +0100

    Task-54816: Implement first part of mail integration settings portlet  (#2)

---
+++ mail-integration-webapp/src/main/webapp/vue-app/mail-integration-settings/main.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import './initComponents.js';
+
+// get overridden components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('MailIntegrationSettings');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
+
+Vue.use(Vuetify);
+const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
+
+document.dispatchEvent(new CustomEvent('displayTopBarLoading'));
+
+const appId = 'MailIntegrationSettingsApplication';
+
+//getting language of the PLF
+const lang = eXo && eXo.env.portal.language || 'en';
+
+//should expose the locale ressources as REST API
+const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.MailIntegration-${lang}.json`;
+
+export function init() {
+  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+    // init Vue app when locale ressources are ready
+    Vue.createApp({
+      mounted() {
+        document.dispatchEvent(new CustomEvent('hideTopBarLoading'));
+      },
+      template: `<mail-integration-settings id="${appId}" />`,
+      vuetify,
+      i18n
+    }, `#${appId}`, 'Mail Integration Settings');
+  });
+}
+
